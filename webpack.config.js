36afe1192dac0d357e8a6249c03d2bc51711e216
hiagomachado777 <hiago.tech@gmail.com>
2018-01-18 21:15:43 -0200

    webpack configurado

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: './app/App.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './public',
+        port: 3333
+    }
+}
