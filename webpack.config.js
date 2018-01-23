04942d5d20eefb73a0f4a3f04effa1665f6e9d2f
hiagomachado777 <hiago.tech@gmail.com>
2018-01-23 10:12:44 -0200

    Atualização no arquivo de configuração do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
+const path = require('path');
+
 module.exports = {
-    entry: './app/App.js',
-    output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        inline: true,
-        contentBase: './public',
-        port: 3333
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }]
-    }
-}
+  devtool: 'source-map',
+  entry: `${__dirname}/app/index.jsx`,
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'app'), 'node_modules'],
+    extensions: [' ', '.js', '.jsx'],
+  },
+  devServer: {
+    inline: true,
+    contentBase: `${__dirname}/public`,
+    port: 3333,
+    historyApiFallback: true,
+  },
+  module: {
+    loaders: [{
+      test: /\.js|.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react', 'stage-0'],
+        plugins: [
+          'transform-decorators-legacy',
+          'transform-class-properties',
+        ],
+      },
+    }],
+  },
+};
